67a999770e3c98bf6fe5a5154992c8837db3df69
Varun Kothari <varun.kothari007@gmail.com>
2021-12-05 17:38:18 +0530

    initial commit

---
+++ lib/zipcodes_usa.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'zipcodes_usa/version'
+require 'yml'
+
+module ZipcodesUsa
+  # WorkerScrape Service
+  class Error < StandardError; end
+  # Your code goes here...
+end
