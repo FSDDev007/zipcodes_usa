9cf8efcc96d29d5c0c494d9caa1cfe2d58726e9b
Varun Kothari <varun.kothari007@gmail.com>
2021-12-17 12:37:37 +0530

    customize methods code

--- lib/zipcodes_usa.rb
+++ lib/zipcodes_usa.rb
@@ -3,51 +3,11 @@
 require 'zipcodes_usa/version'
 require 'yaml'
 require 'zipcodes_usa/query'
-# require 'pry'
 
 module ZipcodesUsa
   # WorkerScrape Service
   class Error < StandardError; end
 
   class Data < Query
-    # def initialize(code = nil, state: nil, city: nil, coverage: nil)
-    #   @code  = code
-    #   @state = state
-    #   @city  = city
-    #   @coverage = coverage
-    # end
-
-    # def details(code = nil)
-    #   code = @code if code.nil?
-    #   Query.find_details(code)
-    # end
-
-    # def fetch_cities(city: nil)
-    #   city = @city if city.nil?
-    #   Query.select_city(city: city)
-    # end
-
-    # def fetch_states(state: nil)
-    #   state = @state if state.nil?
-    #   Query.select_state(state: state)
-    # end
-
-    # def fetch_five_g_coverage(coverage: nil)
-    #   coverage = @coverage if coverage.nil?
-    #   Query.select_five_g_coverage(coverage)
-    # end
   end
 end
-
-# csv = File.read('t_mobile_availability1.csv')
-# CSV.parse(csv, headers: true).each do |row|
-#   puts row.to_h # print all zipcode data in hash formate
-# end
-
-# gem build zipcodes_usa && gem install zipcodes_usa && irb
-# require 'zipcodes_usa'
-# ZipcodesUsa::Find.new
-# ZipcodesUsa::Find.new('12345').details
-# ZipcodesUsa::Find.new(city: 'Anchorage').fetch_cities
-# ZipcodesUsa::Find.new(state: 'AK').fetch_states
-# ZipcodesUsa::Find.new(coverage: 'false').fetch_five_g_coverage
--- lib/zipcodes_usa/query.rb
+++ lib/zipcodes_usa/query.rb
@@ -1,37 +1,37 @@
 # frozen_string_literal: true
 
 module ZipcodesUsa
-  # Query
   class Query
     class << self
-      def select(city: nil, state: nil, code: nil)
+      def select(city: nil, state: nil, postal_code: nil)
         zipcode_list = data_list
 
         zipcode_list = zipcode_list.select { |x| x['city'] == city } unless city.nil?
         zipcode_list = zipcode_list.select { |x| x['state'] == state } unless state.nil?
-        zipcode_list = zipcode_list.select { |x| x['code'] == code } unless code.nil?
+        zipcode_list = zipcode_list.select { |x| x['postal_code'] == postal_code } unless postal_code.nil?
+
+        # %w[city state code].each do |item|
+        #   next unless item.nil?
+
+        #   zipcode_list = zipcode_list.select { |x| x[item] == eval(item) }
+        # end
 
         zipcode_list
       end
 
-      # def select_city(city: nil)
-      #   zipcode_list = YAML.load_file('zipcode.yaml')
-      #   zipcode_list.select { |x| x['city'] == city }
-      # end
+      def cities(postal_code: nil, state: nil)
+        zipcode_list = data_list
 
-      # def select_state(state: nil)
-      #   zipcode_list = YAML.load_file('zipcode.yaml')
-      #   zipcode_list.select { |x| x['state'] == state }
-      # end
+        zipcode_list = zipcode_list.select { |x| x['state'] == state } unless state.nil?
+        zipcode_list = zipcode_list.select { |x| x['postal_code'] == postal_code } unless postal_code.nil?
 
-      # def select_five_g_coverage(coverage)
-      #   zipcode_list = YAML.load_file('output.yaml')
-      #   zipcode_list.select { |x| x['5G'] == coverage }
-      # end
+        # zipcode_list.pluck(:city)
+        zipcode_list.map { |x| x['city'] }
+      end
 
-      def details(code)
-        zipcode_list = YAML.load_file('zipcode.yaml')
-        zipcode_list[code.to_i]
+      def details(postal_code)
+        zipcode_list = data_list
+        zipcode_list[postal_code.to_i]
       rescue StandardError
         {}
       end
